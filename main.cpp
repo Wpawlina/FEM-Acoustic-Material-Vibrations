f2be3944914f38a53614d627de15719126edfe7a
Wpawlina <wpawlina@student.agh.edu.pl>
2024-12-18 18:40:21 +0100

    created the solution

---
+++ main.cpp
@@ -0,0 +1,167 @@
+#include <iostream>
+#include <functional>
+#include <cmath>
+#include <Eigen/Dense> 
+#include <fstream>
+
+
+
+
+using namespace std;
+using namespace Eigen;
+
+
+const int L = 2;
+int n=0;
+double h = 0;
+
+
+double rescale(double x, double a, double b) {
+    return ((b - a) * x + (b + a)) / 2.0;
+}
+
+double integral(function<double(double)>f,double a, double b){
+    double nodes[2] = {-0.57735, 0.57735 };
+    double weights[2] = {1, 1};
+    double integral = 0;
+    
+    for (int i = 0; i < 2; i++) {
+        integral += weights[i] * f(rescale(nodes[i], a, b));
+    }
+    
+    return integral;  
+}
+
+
+
+
+double e_i(int i, double x){
+    if (( h*(i-1) <= x ) && (x < h*i))
+       return x/h - i + 1;
+    else if (( h*(i) <= x ) && (x <= h*(i+1)))
+       return -x/h + i + 1;
+    else
+       return 0;
+}
+
+
+
+double e_i_d(int i, double x){
+    if (( h*(i-1) <= x ) && (x < h*i))
+       return 1/h ;
+    else if (( h*(i) <= x ) && (x <= h*(i+1)))
+       return -1/h ;
+    else
+       return 0;
+}
+
+double B_i_j(int i ,int j){
+    double a=0;
+    double b=0;
+    if( i == j-1)
+    {
+        a=i*h;
+        b=(i+1)*h;
+    }
+    else if(i == j+1)
+    {
+        a=(i-1)*h;
+        b=i*h;
+    }
+    else if(i ==n )
+    {
+        a=(i-1)*h;
+        b=i*h;
+    }
+    else
+    {
+        a=(i-1)*h;
+        b=(i+1)*h;
+    }
+
+  return (-1)*e_i(i,2)*e_i(j,2)+integral([i,j]( double x){return e_i_d(i,x) *e_i_d(j,x);},a,b)-integral([i,j](double x){ return e_i(i,x)*e_i(j,x);},a,b);
+}
+
+double L_j(int j){
+    
+    auto sin1=[](double x){return sin(x);};
+    double a=(j-1)*h;
+    double b=(j+1)*h;
+
+    return integral(sin1,a,b)-integral([j](double x){return e_i_d(j,x)*e_i_d(0,x);},a,b)+integral([j](double x){return e_i(j,x)*e_i(0,x);},a,b)+4*e_i(j,2);
+}
+
+
+
+int main(){
+    cout << "Podaj n: ";
+    cin >> n;
+  
+    if(n<1){
+        cout<<"n musi byc wieksze od 0"<<endl;
+        return 0;
+    }
+
+    h=(double)L/ (double)n;
+
+
+    MatrixXd A(n,n);
+    VectorXd B(n);
+
+    for(int j=0;j<n;j++){
+        for(int i=0;i<n;i++){
+            A(j,i)=B_i_j(i+1,j+1);
+        }
+        B(j)=L_j(j+1);
+    }
+
+
+    VectorXd X = A.colPivHouseholderQr().solve(B);
+    VectorXd X_u(n+1);
+     
+    X_u(0)=0;
+    for(int i=0;i<n;i++){
+        X_u(i+1)=X(i);
+    }
+    for(int i=0;i<n+1;i++){
+        X_u(i)=X_u(i)+e_i(0,i*h);
+    }
+
+    double x[n+1];
+    for(int i=0;i<n+1;i++){
+        x[i]=i*h;
+    }
+    double y[n+1];
+    for(int i=0;i<n+1;i++){
+        y[i]=X_u(i);
+    }
+    
+    fstream file;
+
+    file.open("data.txt",ios::out);
+    for(int i=0;i<n+1;i++){
+        file<<x[i]<<" "<<y[i]<<endl;
+       
+    }
+
+    file.close(); 
+
+
+
+
+
+
+  
+   
+    return 0;
+
+
+    
+
+  
+
+  
+
+
+ 
+}
---
+++ readme.md
@@ -0,0 +1,26 @@
+Finite-Element-Method-for-Differential-and-Difference-Equations
+Implementation and Analysis of Finite Element Method for Differential and Difference Equations of Acoustic Material Vibrations
+================================================
+
+Algorithm solves differential equation with boundary conditions using finite element method. It finds Equation of Acoustic Material Vibrations with provided by the user precision. 
+It was a part of AGH UST Differential and Difference Equations subject.
+
+
+Technologies used
+================================================
+C++ - Eigen for matrix calculations
+
+Setup and launch
+================================================
+<ul>
+    <li>make sure you have installed a c++ compiler</li>
+    <li>navigate to project directory</li>
+    <li>compile main.cpp with youf compiler and include Eigen library during compilation</li>
+    <li>excecute created  .exe file</li>
+    <li>enter expected precision</li>
+</ul>
+Results will be save in data.txt file.
+
+
+
+
